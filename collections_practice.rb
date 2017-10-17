5f540e327b1c2c062826758209f83f5027fd343a
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-17 15:19:33 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,4 +2,12 @@ def sort_array_desc(integers)
   new_integers = []
   integers.sort do |a,b|
     if a == b
+      0
+    elsif a < b
+      -1
+    elsif a > b
+      1
+    end
+  end
+  new_integers
 end
