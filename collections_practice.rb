be5081bbe5e0aee04734b8cf75c7034db94cd584
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-17 16:36:47 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -32,9 +32,7 @@ def kesha_maker(kesha)
 end
 
 def find_a(strings)
-  a_strings = []
   strings.select do |i|
-    a_strings << i.starts_with? a
+    a_strings << i.starts_with? "a"
   end
-  a_strings
 end
